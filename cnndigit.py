915fbdb7cf748af62df835fc1af648519970c6af
Alireza <ahashemi1375@gmal.com>
2018-12-02 00:08:46 +0330

    Updated on Dec 1

--- cnndigit.py
+++ cnndigit.py
@@ -1,31 +1,22 @@
-import numpy as np
+from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D
+from keras.models import Model, Sequential
+from keras.utils import to_categorical
 from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.utils import np_utils
-from keras import backend as K
 from HodaDatasetReader import read_hoda_dataset
-import matplotlib.pyplot as plt
-
-K.set_image_dim_ordering('th')
 
 # FARSI Dataset
-size = 32
-X_train, y_train = read_hoda_dataset('./samples-FA/Train 60000.cdb', images_height=size,
-                                     images_width=size,
-                                     one_hot=False,
-                                     reshape=False)
-X_test, y_test = read_hoda_dataset('./samples-FA/Test 20000.cdb', images_height=size,
-                                   images_width=size,
-                                   one_hot=False,
-                                   reshape=False)
+x_train_fa, y_train_fa = read_hoda_dataset('./samples-FA/Train 60000.cdb', images_height=32,
+                                           images_width=32,
+                                           one_hot=False,
+                                           reshape=False)
+x_test_fa, y_test_fa = read_hoda_dataset('./samples-FA/Test 20000.cdb', images_height=32,
+                                         images_width=32,
+                                         one_hot=False,
+                                         reshape=False)
+
 # ENGLISH Dataset
-# size=28
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+(x_train_en, y_train_en), (x_test_en, y_test_en) = mnist.load_data()
+
 '''
 plt.subplot(221)
 plt.imshow(X_train[100], cmap=plt.get_cmap('gray'))
@@ -38,36 +29,54 @@ plt.imshow(X_train[10000], cmap=plt.get_cmap('gray'))
 # show the plot
 plt.show()
 '''
-seed = 7
-np.random.seed(seed)
-X_train = X_train.reshape(X_train.shape[0], 1, size, size).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, size, size).astype('float32')
+x_train_fa = x_train_fa.reshape(60000, 32, 32, 1)
+x_test_fa = x_test_fa.reshape(20000, 32, 32, 1)
+x_train_fa = x_train_fa.astype('float32') / 255
+x_test_fa = x_test_fa.astype('float32') / 255
+y_train_fa = to_categorical(y_train_fa, 10)
+y_test_fa = to_categorical(y_test_fa, 10)
 
-X_train = X_train / 255
-X_test = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+x_train_en = x_train_en.reshape(60000, 28, 28, 1)
+x_test_en = x_test_en.reshape(10000, 28, 28, 1)
+x_train_en = x_train_en.astype('float') / 255
+x_test_en = x_test_en.astype('float') / 255
+y_train_en = to_categorical(y_train_en, 10)
+y_test_en = to_categorical(y_test_en, 10)
+
+size = (28, 28, 1)
 
 
-def baseline_model():
-    model = Sequential()
-    model.add(Conv2D(32, (5, 5), input_shape=(1, size, size), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-    # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+def build_model():
+    inp = Input(shape=size)
+    L1 = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size,))(inp)
+    L2 = MaxPool2D(pool_size=(2, 2))(L1)
+    L3 = Flatten()(L2)
+    model = Model(inp, L3)
+    model.compile(optimizer='sgd', loss='mse')
     return model
 
 
-# build the model
-model = baseline_model()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=128, verbose=1)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("CNN Error: %.2f%%" % (100 - scores[1] * 100))
+myModel = build_model()
+
+en_model = Sequential()
+en_model.add(myModel)
+en_model.add(Dense(10, activation='softmax'))
+en_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+en_model.fit(x_train_en, y_train_en, batch_size=128, epochs=5, validation_split=0.2)
+scores_en = en_model.evaluate(x_test_en, y_test_en)
+
+
+# myModel.trainable = False
+
+x_train_fa = x_train_fa[:, 2:30, 2:30, :]
+x_test_fa = x_test_fa[:, 2:30, 2:30, :]
+
+fa_model = Sequential()
+#fa_model.add(MaxPool2D(pool_size=(1,1), padding='same'))
+fa_model.add(myModel)
+fa_model.add(Dense(10, activation='softmax'))
+fa_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+fa_model.fit(x_train_fa, y_train_fa, batch_size=128, epochs=5, validation_split=0.2)
+scores_fa = fa_model.evaluate(x_test_fa, y_test_fa)
