d14e50006c414bdbe9319567bef0075e6ca4035d
Alireza <ahashemi1375@gmail.com>
2018-12-02 22:26:28 +0330

    Better CNN

--- cnndigit.py
+++ cnndigit.py
@@ -1,16 +1,16 @@
-from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D
+from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D, Dropout
 from keras.models import Model, Sequential
 from keras.utils import to_categorical
 from keras.datasets import mnist
 from HodaDatasetReader import read_hoda_dataset
-
+import keras as K
 # FARSI Dataset
-x_train_fa, y_train_fa = read_hoda_dataset('./samples-FA/Train 60000.cdb', images_height=32,
-                                           images_width=32,
+x_train_fa, y_train_fa = read_hoda_dataset('./samples-FA/Train 60000.cdb', images_height=28,
+                                           images_width=28,
                                            one_hot=False,
                                            reshape=False)
-x_test_fa, y_test_fa = read_hoda_dataset('./samples-FA/Test 20000.cdb', images_height=32,
-                                         images_width=32,
+x_test_fa, y_test_fa = read_hoda_dataset('./samples-FA/Test 20000.cdb', images_height=28,
+                                         images_width=28,
                                          one_hot=False,
                                          reshape=False)
 
@@ -29,10 +29,10 @@ plt.imshow(X_train[10000], cmap=plt.get_cmap('gray'))
 # show the plot
 plt.show()
 '''
-x_train_fa = x_train_fa.reshape(60000, 32, 32, 1)
-x_test_fa = x_test_fa.reshape(20000, 32, 32, 1)
-x_train_fa = x_train_fa.astype('float32') / 255
-x_test_fa = x_test_fa.astype('float32') / 255
+x_train_fa = x_train_fa.reshape(60000, 28, 28, 1)
+x_test_fa = x_test_fa.reshape(20000, 28, 28, 1)
+x_train_fa = x_train_fa.astype('float') / 255
+x_test_fa = x_test_fa.astype('float') / 255
 y_train_fa = to_categorical(y_train_fa, 10)
 y_test_fa = to_categorical(y_test_fa, 10)
 
@@ -46,37 +46,42 @@ y_test_en = to_categorical(y_test_en, 10)
 size = (28, 28, 1)
 
 
-def build_model():
-    inp = Input(shape=size)
-    L1 = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size,))(inp)
-    L2 = MaxPool2D(pool_size=(2, 2))(L1)
-    L3 = Flatten()(L2)
-    model = Model(inp, L3)
-    model.compile(optimizer='sgd', loss='mse')
+def build_model(size_):
+    inp = Input(shape=size_)
+    L1 = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size_,))(inp)
+    L2 = Conv2D(64, kernel_size=(3, 3), activation='relu')(L1)
+    L3 = MaxPool2D(pool_size=(2, 2))(L2)
+    L4 = Dropout(0.25)(L3)
+    L5 = Flatten()(L4)
+    model = Model(inp, L5)
+    model.compile(optimizer='adam', loss='mse')
     return model
 
 
-myModel = build_model()
+myModel = build_model((28, 28, 1))
 
 en_model = Sequential()
 en_model.add(myModel)
+en_model.add(Dense(128, activation='relu'))
+en_model.add(Dropout(0.5))
 en_model.add(Dense(10, activation='softmax'))
-en_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+en_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
 
-en_model.fit(x_train_en, y_train_en, batch_size=128, epochs=5, validation_split=0.2)
+en_model.fit(x_train_en, y_train_en, batch_size=128, epochs=5, validation_data=(x_test_en, y_test_en))
 scores_en = en_model.evaluate(x_test_en, y_test_en)
 
+#myModel.trainable = False
 
-# myModel.trainable = False
-
-x_train_fa = x_train_fa[:, 2:30, 2:30, :]
-x_test_fa = x_test_fa[:, 2:30, 2:30, :]
-
+#myModel2 = build_model((28, 28, 1))
 fa_model = Sequential()
-#fa_model.add(MaxPool2D(pool_size=(1,1), padding='same'))
 fa_model.add(myModel)
+fa_model.add(Dense(128, activation='relu'))
+fa_model.add(Dropout(0.5))
 fa_model.add(Dense(10, activation='softmax'))
-fa_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+fa_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
 
-fa_model.fit(x_train_fa, y_train_fa, batch_size=128, epochs=5, validation_split=0.2)
+fa_model.fit(x_train_fa, y_train_fa, batch_size=128, epochs=5, validation_data=(x_test_fa, y_test_fa))
 scores_fa = fa_model.evaluate(x_test_fa, y_test_fa)
+
+print('English Model Train Score: %.2f' % (100.0 - scores_en[0]*100))
+print('Farsi Model Train Score: %.2f' % (100.0 - scores_fa[0]*100))
