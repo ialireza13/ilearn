efbcac4a99448a3f85ac3a87bdf6630a214dd2a9
Alireza <ahashemi1375@gmail.com>
2018-12-08 23:11:12 +0330

    Dec. 8

--- cnndigit.py
+++ cnndigit.py
@@ -2,6 +2,7 @@ from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D, Dropout
 from keras.models import Model, Sequential
 from keras.utils import to_categorical
 from keras.datasets import mnist
+from keras.models import model_from_json
 from HodaDatasetReader import read_hoda_dataset
 import keras as K
 # FARSI Dataset
@@ -58,30 +59,55 @@ def build_model(size_):
     return model
 
 
-myModel = build_model((28, 28, 1))
+myModel2 = build_model((28, 28, 1))
 
-en_model = Sequential()
-en_model.add(myModel)
-en_model.add(Dense(128, activation='relu'))
-en_model.add(Dropout(0.5))
-en_model.add(Dense(10, activation='softmax'))
-en_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
-
-en_model.fit(x_train_en, y_train_en, batch_size=128, epochs=5, validation_data=(x_test_en, y_test_en))
-scores_en = en_model.evaluate(x_test_en, y_test_en)
-
-#myModel.trainable = False
-
-#myModel2 = build_model((28, 28, 1))
 fa_model = Sequential()
-fa_model.add(myModel)
+fa_model.add(myModel2)
 fa_model.add(Dense(128, activation='relu'))
 fa_model.add(Dropout(0.5))
 fa_model.add(Dense(10, activation='softmax'))
 fa_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
 
-fa_model.fit(x_train_fa, y_train_fa, batch_size=128, epochs=5, validation_data=(x_test_fa, y_test_fa))
+fa_model.fit(x_train_fa, y_train_fa, batch_size=128, epochs=10, validation_data=(x_test_fa, y_test_fa))
 scores_fa = fa_model.evaluate(x_test_fa, y_test_fa)
 
-print('English Model Train Score: %.2f' % (100.0 - scores_en[0]*100))
-print('Farsi Model Train Score: %.2f' % (100.0 - scores_fa[0]*100))
+myModel2.trainable = False
+
+en_model = Sequential()
+en_model.add(myModel2)
+en_model.add(Dense(128, activation='relu'))
+en_model.add(Dropout(0.5))
+en_model.add(Dense(10, activation='softmax'))
+en_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
+
+en_model.fit(x_train_en, y_train_en, batch_size=128, epochs=10, validation_data=(x_test_en, y_test_en))
+scores_en = en_model.evaluate(x_test_en, y_test_en)
+
+print('English Model Train Accuracy: %.2f' % (scores_en[1]*100))
+print('Farsi Model Train Accuracy: %.2f' % (scores_fa[1]*100))
+
+'''
+json_file = open('fa_model.json', 'r')
+fa_loaded_model_json = json_file.read()
+json_file.close()
+fa_loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+fa_loaded_model.load_weights("fa_model.h5")
+print("Loaded model from disk")
+fa_loaded_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
+fa_load_score = fa_loaded_model.evaluate(x_test_fa, y_test_fa)
+
+json_file = open('en_model_3.json', 'r')
+en_loaded_model_json = json_file.read()
+json_file.close()
+en_loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+en_loaded_model.load_weights("en_model_3.h5")
+print("Loaded model from disk")
+en_loaded_model.compile(optimizer=K.optimizers.Adadelta(), loss=K.losses.categorical_crossentropy,metrics=['accuracy'])
+en_load_score = en_loaded_model.evaluate(x_test_en, y_test_en)
+
+
+print('LOAD: English Model Train Accuracy: %.2f' % (en_load_score[1]*100))
+print('LOAD: Farsi Model Train Accuracy: %.2f' % (fa_load_score[1]*100))
+'''
